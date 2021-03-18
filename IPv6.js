0ea2638e480d18a23cb34ada66ee99bdf182fc67
Pranjali163 <pranjali.damle@lumen.com>
2021-03-18 11:47:22 +0000

    Lab 3

--- IPv6.js
+++ IPv6.js
@@ -45,5 +45,6 @@ function getIpv4MappedIpv6Address(ipv4) {
     }
   }
   return ipv6Address;
+  module.exports.getIpv4MappedIpv6Address = getIpv4MappedIpv6Address;
 }
-module.exports.getIpv4MappedIpv6Address = getIpv4MappedIpv6Address;
+
